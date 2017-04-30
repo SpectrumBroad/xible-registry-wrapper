888fb08f3480915770d657a4eae26c006257aa9f
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-04-30 14:10:57 +0200

    strip trailing slash from urls

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ class XibleRegistryWrapper {
 
 		if (obj && obj.url) {
 			this.url = obj.url;
+
+			if(this.url.substring(this.url.length - 1) === '/') {
+				this.url = this.url.substring(0, this.url.length - 1);
+			}
 		}
 
 		//this.Flow = require('./Flow.js')(this);
